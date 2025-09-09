9f2e6eeae5d1d14309adbb2449326bbf78b34c3c
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-09-09 07:40:57 +0700

    data movement

--- week 2/dataMove.cpp
+++ week 2/dataMove.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
 using namespace std;
 
-void MovingDown (int x[], int index, int lastindex){
-    int temp = lastindex;
-        while (temp == index){
-            x[temp] = x[temp-1];
-            temp--;
-        }
-}
-
-void MovingUp (int z [], int total){
-    for (int i=0;i<total;i++){
-        if (i == total-1)
-            cout << z[i] << endl;
-        else
-            cout << z[i] << ", ";
-        // (i ==(total-1))? cout << z[i] :cout << z[i]<< ", ";
-        // cout << endl;
+void MovingRight(int arr[], int currentSize, int insertIndex){
+    for (int i = currentSize; i > insertIndex; i--){
+        arr[i] = arr [i-1];
     }
 }
 
+void printArray (int arr[], int size){
+    for (int i = 0; i < size; i++){
+        cout << arr[i];
+        if (i < size - 1){
+            cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
 int main(){
+    const int CAPACITY = 10;
+    int number[CAPACITY];
+    int currentSize = 0;
+
+    cout <<"input 10 numbers: "<< endl;
+    cout <<"input how many numbers that you want to insert: "<< "maximal is "<< CAPACITY<< endl;
+    cin >> currentSize;
+
+    while (currentSize > CAPACITY || currentSize < 0){
+        cout << "input again: "<< endl;
+        cin >> currentSize;
+    }
+
+        cout << "please intput the numbers: " << endl;
+    for (int i = 0 ; i < currentSize; i++){
+        cout << "number of "<< i +1 << ": ";
+        cin >> number[i];
+    }
+
+    cout << "\n array before moving: " << endl;
+    printArray(number, currentSize);
+
+    if (currentSize < CAPACITY){
+        cout << "\n new storage of array: " << endl;
+        int valueInsert, insertIndex;
+
+        cout << "input the number that you want to insert: ";
+        cin >> valueInsert;
+
+        cout << "input the index that you want to insert: ";
+        cin >> insertIndex;
+    }
+
 }
