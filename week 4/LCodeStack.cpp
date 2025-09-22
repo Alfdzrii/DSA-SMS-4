eccbcfd897bda2ad72ed7a4a1d497ed73d40fad5
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-09-22 15:59:13 +0700

    new stack linked list (lecturer code)

--- week 4/LCodeStack.cpp
+++ week 4/LCodeStack.cpp
@@ -1,73 +1,71 @@
 #include <iostream>
 using namespace std;
-
-struct LCodeStack
-{
-    int data;
-    LCodeStack *next;
+struct Lstack{
+	int data;
+	Lstack *next;
 };
-
 int menu()
 {
-    int ans;
-    cout<<"input the number that you want to insert: \n";
-    cout<<"1. insert\n";
-    cout<<"2. delete\n";
-    cout<<"3. exit\n";
-    cin >> ans;
-    return ans;
+  int ans;
+	cout << " --Menu--\n";
+	cout << "1. Insert\n";
+	cout << "2. Delete\n";
+	cout << "3. Exit\n";
+	cout << "Select your choice: ";
+	cin >> ans;
+	return ans;
 }
-
-LCodeStack *insert (LCodeStack *frnt, int value){
-    LCodeStack *temp = new (LCodeStack);
-    temp->data = value;
-    temp->next = frnt;
-    frnt = temp;
-    return frnt;
-
-};
-
-LCodeStack *Delete (LCodeStack *frnt){
-    LCodeStack *temp = frnt;
-    frnt = frnt->next;
-    delete(temp);
-    return frnt;
-};
-
-void printData(LCodeStack *frnt){
-    LCodeStack *cur = frnt;
-    while (cur) {
-        cout << cur ->data << " ";
-        cur = cur->next;
-    }
-    cout << endl;
+Lstack *Insert(Lstack *frnt,int x)
+{
+	Lstack *temp = new(Lstack);
+	temp->data = x;
+	temp->next = frnt;
+	return temp;
 }
-
-int main(){
-    LCodeStack *front =0;
-    int data,choice;
-
-    char ans[2]={1,1};
-    do {
-        choice = menu();
-        switch (choice)
-        {
-        case 1:
-            cout << "insrt data \n";
-            cin >> data ;
-            front = insert (front, data);
-            printData(front);
-            break;
-        case 2:
-            Delete(front);
-            printData(front);
-            break;
-        case 3:
-            ans[0]=0;
-            break;
-
-        }
-    }
-    while (ans[0]);
-    
+Lstack *Delete(Lstack *frnt)
+{
+	if(frnt != 0){
+	    Lstack *temp = frnt;
+	    frnt = frnt->next;
+	    delete(temp);
+	}else cout << "No data to be deleted!!!\n";
+	return frnt;
+}
+void PrintData(Lstack *frnt)
+{
+  Lstack *cur = frnt;
+  while(cur){
+      cout << cur->data << " ";
+      cur = cur->next;
+  }
+  cout << endl;
+}
+int main()
+{
+Lstack *front = 0;
+int data,choice;
+char ans[2];
+	do{
+		choice = menu();
+		switch(choice){
+			case 1:
+				cout << "Put the data to be inserted: ";
+				cin >> data;
+				front = Insert(front,data);
+				break;
+			case 2:
+				front = Delete(front);
+				break;
+			case 3:
+				ans[0] = 'N';
+				break;
+		}
+		if(choice != 3){
+			cout << "The sequence is: \n";
+			PrintData(front);
+		}
+		cout << "Press y to continue: ";
+		cin >> ans;
+	}while(ans[0] == 'y' || ans[0] == 'Y');
+	return 0;
 }
